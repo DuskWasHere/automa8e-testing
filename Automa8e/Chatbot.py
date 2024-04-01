e9d0e4882a45000f8a48fee2902c0877dfad2890
Ellesia-Ysha <120109383+Ellesia-Ysha@users.noreply.github.com>
2024-04-02 00:00:11 +0800

    Adjusted chatbot design

--- Automa8e/Chatbot.py
+++ Automa8e/Chatbot.py
@@ -7,20 +7,8 @@ import altair as alt
 from streamlit_lottie import st_lottie
 from streamlit_gsheets import GSheetsConnection
 from utils.google_sheets import handle_data_refresh
-from utils.google_sheets import handle_data_refresh
 
 st.set_page_config(page_title="Automa8e", layout = "wide", page_icon = "images\page icon.png")
-
-# logo
-logo = Image.open("images\logo (6).png")
-st.image(logo, width=250)
-
-# Function to fetch data and cache it
-@st.cache_data(ttl=300)
-def fetch_data():
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    data = conn.read(worksheet="Support")
-    return data
   
 handle_data_refresh()
 
@@ -36,24 +24,31 @@ anim = load_anim("https://lottie.host/81a6ce70-e829-4023-8394-4c659bafe651/HWw7G
 # chatbot embedding 
 def embed_chatbot():
     components.html("""
-<script type="text/javascript">
-  (function(d, t) {
-    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-    v.onload = function() {
-      window.voiceflow.chat.load({
-        verify: { projectID: '65f8348ce18439e83b8f6611' },
-        url: 'https://general-runtime.voiceflow.com',
-        versionID: 'production'
-      }).then(() => {
-        window.voiceflow.chat.open();
-      });
-    };
-    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
-  })(document, 'script');
+<div style="width: 100%; height: 800px; margin: 0; padding: 0;" id="VG_OVERLAY_CONTAINER">
+    <!-- Here is where Voiceglow renders the widget. -->
+    <!-- Set render to 'full-width' then adjust the width and height to 500px (for example) to render the chatbot itself without the popup. -->
+</div>
+
+<!-- Remove 'defer' if you want widget to load faster (Will affect website loading) -->
+<script defer>
+    (function() {
+        window.VG_CONFIG = {
+            ID: "z6f8rrkg9",
+            region: 'na', // 'eu' or 'na' corresponding to Europe and North America
+            render: 'full-width', // Set render to 'full-width' to load chatbot automatically
+            stylesheets: [
+                // Base Voiceglow CSS
+                "https://storage.googleapis.com/voiceglow-cdn/vg_live_build/styles.css",
+                // Add your custom css stylesheets, Can also add relative URL ('/public/your-file.css)
+            ],
+        }
+        var VG_SCRIPT = document.createElement("script");
+        VG_SCRIPT.src = "https://storage.googleapis.com/voiceglow-cdn/vg_live_build/vg_bundle.js";
+        document.body.appendChild(VG_SCRIPT);
+    })();
 </script>
 """, height=800)
-
-
+    
 # main body
 with st.container():
   left_column, right_column = st.columns(2)
